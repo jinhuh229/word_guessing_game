921957dc6affd38d27290e0c1da669f8f6d8fed1
Jin Huh <37564888+jinhuh229@users.noreply.github.com>
2019-05-11 10:38:54 -0400

    Add files via upload

--- word_gussing_game/src/word_gussing_game/LoginController.java
+++ word_gussing_game/src/word_gussing_game/LoginController.java
@@ -36,7 +36,7 @@ public class LoginController {
     Label exit_text;
 
 
-
+    @FXML
     public void login_clicked() throws SQLException {
 
         PreparedStatement preparedStatement = null;
@@ -75,6 +75,10 @@ public class LoginController {
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
+
+
+                Stage stage = (Stage) login_button.getScene().getWindow();
+                stage.close();
             }
 
         } catch (SQLException ex) {
@@ -85,7 +89,7 @@ public class LoginController {
 
 
         // ---------------------------------   No Account Mouse Clicked (Label) --------------------------------------------
-
+        @FXML
         public void no_account_clicked(){
 
             try {
@@ -100,10 +104,79 @@ public class LoginController {
         }
 
         // ---------------------------------   Close the window when click the X  (Label) --------------------------------------------
+        @FXML
         public void close_window(){
             Stage stage = (Stage) exit_text.getScene().getWindow();
             stage.close();
         }
+
+
+    // ---------------------------------   Pressed the enter to login --------------------------------------------
+        @FXML
+        public void pressed_enter(ActionEvent ae){
+            PreparedStatement preparedStatement = null;
+            String query = "SELECT * FROM login WHERE userName =? and password =?";
+
+
+            String userName = username_text.getText();
+            String password = password_text.getText();
+
+            try {
+
+
+                Connection con = DBConnection.getConnection();
+                preparedStatement = con.prepareStatement(query);
+
+
+                preparedStatement.setString(1, userName);
+                preparedStatement.setString(2, password);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+
+                if(!resultSet.next()) {
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "User Name or Password is incorrect", ButtonType.CLOSE);
+                    alert.showAndWait();
+                }
+                else {
+
+
+                    try {
+                        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("main_game_screen.fxml"));
+                        Parent root1 = (Parent) fxmlLoader.load();
+                        Stage stage = new Stage();
+                        stage.setScene(new Scene(root1));
+                        stage.show();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+
+            Stage stage = (Stage) login_button.getScene().getWindow();
+            stage.close();
+
+        }
+
+
+    // ---------------------------------   get information about account  --------------------------------------------
+        @FXML
+        public String getInformaiton(){
+
+
+            String getuserName = username_text.getText();
+
+            System.out.println(getuserName);
+
+            return getuserName;
+
+
+        }
+
+
 
     }
 
--- word_gussing_game/src/word_gussing_game/MainGameScreenController.java
+++ word_gussing_game/src/word_gussing_game/MainGameScreenController.java
@@ -1,4 +1,29 @@
 package word_gussing_game;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MainGameScreenController {
-}
+
+    @FXML
+    Label display_id;
+
+
+    public void show_account_for_user(){
+
+
+        }
+
+
+    @FXML
+    public void initialize() {
+
+        // display the account userID
+
+    }
+
+    }
+
+
+
+
